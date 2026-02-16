8412da0cb6515c6ac10424fa7fffc000e5e03197
zemedkunworkalem32 <workalemzame@gmail.com>
2026-02-16 12:24:41 +0300

    and the read keyboard and updatea the first file

--- First.java
+++ First.java
@@ -1,6 +1,11 @@
 public class First {
   public static void main(String[] arg){
     System.out.println("Hello World!");
+    System.out.println(arg[0]);
+    System.out.println(arg[1]);
+    System.out.println("The Land Of Origin! ");
+    System.out.println(arg[2]);
+
 
   }
 
---
+++ ReadKeyboard.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class ReadKeyboard {
+  public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+        String name;
+          System.out.print("Enter your name: ");
+           name = scanner.nextLine();
+          System.out.println("Hello, " + name + "!");
+
+      scanner.useRadix(2);
+      int x = scanner.nextInt();
+      System.out.println(x);
+
+      scanner.close();
+  }
+  
+}
